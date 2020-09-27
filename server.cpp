247fc1e7523d2fc0603361ce84174ab52ba724c8
KacperLapiak <68159699+KacperLapiak@users.noreply.github.com>
2020-09-27 16:09:34 +0200

    Add files via upload

---
+++ server.cpp
@@ -0,0 +1,55 @@
+﻿/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> // This header gives access to number of system services.
+#include <sys/types.h> // This header file contains definitions of a number of data types used in system calls. These types are used in the next two include files.
+#include <sys/socket.h> // The header file socket.h includes a number of definitions of structures needed for sockets.
+#include <netinet/in.h> // The header file in.h contains constants and structures needed for internet domain addresses.
+
+void error(const char* msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr*)&serv_addr,
+        sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd,
+        (struct sockaddr*)&cli_addr,
+        &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    bzero(buffer, 256);
+    n = read(newsockfd, buffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n", buffer);
+    n = write(newsockfd, "I got your message", 18);
+    if (n < 0) error("ERROR writing to socket");
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
